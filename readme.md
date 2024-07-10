7350a46930d9998e091494cafe53abe6dc5913b1
yuanZeNing <yuanzn189@chinatelecom.cn>
2024-07-10 15:36:05 +0800

    测试爬虫

---
+++ readme.md
@@ -0,0 +1,1 @@
+https://github.com/wistbean/learn_python3_spider?tab=readme-ov-file
---
+++ urllibTest.py
@@ -0,0 +1,4 @@
+import urllib.request
+if __name__ == "__main__":
+    response = urllib.request.urlopen('https://juejin.cn/pins/hot')
+    print(response.read().decode('utf-8'))
